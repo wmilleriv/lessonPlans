eb56fb4d7f08fd5147cee2b55a2be22e1ce4be92
William Miller <Wmilleiv@gmail.com>
2025-08-10 13:43:52 -0400

    boiler plate

--- coding/codeSamples/calc.asm
+++ coding/codeSamples/calc.asm
@@ -1,2 +1,11 @@
-welcome .data 0dh,0ah, 0dh, 0ah, " Hello ", 0dh, 0ah
-welcome_length equ $-welcome
+global _start
+
+section .data
+    welcome db 0dh,0ah, 0dh, 0ah, " Hello ", 0dh, 0ah
+    welcome_length equ $-welcome
+
+section .text
+
+_start:
+
+LOOP:
