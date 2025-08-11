965871c6e6b557e06796a7bb79f3a0b5f2dbbcd0
William Miller <Wmilleiv@gmail.com>
2025-08-11 12:23:03 -0400

    welcome function

--- coding/codeSamples/calc.asm
+++ coding/codeSamples/calc.asm
@@ -9,3 +9,11 @@ section .text
 _start:
 
 LOOP:
+
+welcome_message:
+    mov rax, 0x1 ;set to write
+    mov rdi, 1 ;set to stdout
+    mov rsi, welcome ;add string to register
+    mov rdx, welcome_length ;set string length
+    syscall
+    ret `;return
