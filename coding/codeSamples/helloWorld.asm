681c9ab78af627f1df4b316405bd1801a7b27cca
William Miller <Wmilleiv@gmail.com>
2025-08-15 15:22:19 -0400

    Exec format error

--- coding/codeSamples/helloWorld.asm
+++ coding/codeSamples/helloWorld.asm
@@ -1,15 +1,16 @@
-.section .text
-    .global _start
+.data
+hello:
+    .string "Hello world!\n"
+
+.text
+.globl _start
 _start:
-    mov $4,%rax
-    mov $1,%rbx
-    mov $message,%ecx
-    mov msglength,%edx
+    movl $4, %eax # write(1, hello, strlen(hello))
+    movl $1, %ebx
+    movl $hello, %ecx
+    movl $13, %edx
     int  $0x80
 
-    mov $1, %rax
-    mov $0, %ebx
-    int $0x80
-.section .data
-    message db "Hello world!", 0dh, 0ah
-    msglength: equ $-message
+    movl $1, %eax # exit(0)
+    movl $0, %ebx
+    int  $0x80
