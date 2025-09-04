0384f7a750d133851ebc2f702b4a37196f03d762
William Miller <Wmilleiv@gmail.com>
2025-09-04 12:56:23 -0400

    Basic castle crasher programs

---
+++ coding/codeSamples/castleCrashers.py
@@ -0,0 +1,23 @@
+float myVariable;
+
+// "when started" hat block
+int whenStarted1() {
+  Drivetrain.driveFor(forward, 1500.0, mm, true);
+  Drivetrain.turnFor(right, 90.0, degrees, true);
+  Drivetrain.driveFor(forward, 750.0, mm, true);
+  Drivetrain.turnFor(right, 90.0, degrees, true);
+  Drivetrain.driveFor(forward, 1500.0, mm, true);
+  Drivetrain.turnFor(right, 90.0, degrees, true);
+  Drivetrain.driveFor(forward, 1500.0, mm, true);
+  Drivetrain.turnFor(right, 90.0, degrees, true);
+  Drivetrain.driveFor(forward, 1500.0, mm, true);
+  return 0;
+}
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  whenStarted1();
+}
