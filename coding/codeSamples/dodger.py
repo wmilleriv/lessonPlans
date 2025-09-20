74328ed8dd1c304f33c906262536d16e98159b93
William Miller <Wmilleiv@gmail.com>
2025-09-20 18:46:25 -0400

    base

--- coding/codeSamples/dodger.py
+++ coding/codeSamples/dodger.py
@@ -5,5 +5,16 @@ pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock=pygame.time.Clock()
 running=True
+dt=0
+
+player_pos=pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+
+    screen.fill("purple")
 
 
