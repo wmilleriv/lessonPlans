804ec56af3676670579ea7e7a62d55f6ed6caa8b
William Miller <Wmilleiv@gmail.com>
2025-10-07 11:46:28 -0400

    falling enemy

--- coding/codeSamples/dodger.py
+++ coding/codeSamples/dodger.py
@@ -18,12 +18,18 @@ DISPLAY_SURFACE.fill(PURPLE)
 pygame.display.set_caption("Dodger")
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self,color, length, width, x, y):
+    def __init__(self, color, width, height, x, y):
         super().__init__()
-        self.color=color
-        self.image = pygame.Surface((length,width),pygame.SRCALPHA)
-        pygame.draw.rect(self.image, self.color, (length,width,x,y))
-        self.rect=self.image.get_rect(center=(x,y))
+        self.image = pygame.Surface([width, height],pygame.SRCALPHA)
+        self.image.fill(color)
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x,y)
+
+    def move(self):
+        self.rect.move_ip(0,10)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
 
 
 class Player(pygame.sprite.Sprite):
@@ -52,16 +58,18 @@ class Player(pygame.sprite.Sprite):
 
 p1=Player(RED,20,SCREEN_WIDTH//2,SCREEN_HEIGHT//2)
 
-e=Enemy(GREEN,20,50,SCREEN_WIDTH//3,SCREEN_HEIGHT//3)
+e=Enemy(GREEN,10,30,SCREEN_WIDTH//3,SCREEN_HEIGHT//3)
 while True:
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
             sys.exit()
     p1.update()
+    e.move()
 
     DISPLAY_SURFACE.fill(PURPLE)
     p1.draw(DISPLAY_SURFACE)
+    e.draw(DISPLAY_SURFACE)
 
     pygame.display.update()
     pygame.time.Clock().tick(FPS)
